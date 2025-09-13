e0fcdfa16ad4d44b54e915c6a236f1b89fc99036
nonnosuke <168043854+nonnosuke@users.noreply.github.com>
2025-09-14 04:03:51 +0900

    Update test_validation.js

--- selenium-tests/test_validation.js
+++ selenium-tests/test_validation.js
@@ -3,7 +3,7 @@ const {Builder, By, until} = require('selenium-webdriver');
 (async function testValidation() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://<TESTING-WEB-SERVER-IP>/');
+    await driver.get('http://54.83.89.110/');
     await driver.findElement(By.name('name')).sendKeys('Alice');
     // Leave email empty
     await driver.findElement(By.name('role')).sendKeys('Developer');
