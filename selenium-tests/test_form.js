ff3de458cedc86b093281a1200ee5c55c0e54323
nonnosuke <168043854+nonnosuke@users.noreply.github.com>
2025-09-14 04:15:36 +0900

    Update test_form.js

--- selenium-tests/test_form.js
+++ selenium-tests/test_form.js
@@ -2,11 +2,12 @@ const {Builder, By, until} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 (async function testForm() {
- let options = new chrome.Options()
-        .headless() // run without GUI
-        .addArguments('--no-sandbox')
-        .addArguments('--disable-dev-shm-usage')
-        .addArguments('--user-data-dir=/tmp/chrome-user-data-' + Date.now()); // unique temp dir
+   // Create Chrome options
+    let options = new chrome.Options();
+    options.addArguments('--headless');              // run without GUI
+    options.addArguments('--no-sandbox');            // needed for EC2
+    options.addArguments('--disable-dev-shm-usage'); // avoid shared memory issue
+    options.addArguments('--user-data-dir=/tmp/chrome-user-data-' + Date.now()); // unique profile
   
   let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
   try {
