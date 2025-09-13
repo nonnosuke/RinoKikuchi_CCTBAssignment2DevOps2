983c2032560baa8bf7321f300d80cddeb190e582
nonnosuke <168043854+nonnosuke@users.noreply.github.com>
2025-09-14 04:02:49 +0900

    Update test_form.js

--- selenium-tests/test_form.js
+++ selenium-tests/test_form.js
@@ -3,7 +3,7 @@ const {Builder, By, until} = require('selenium-webdriver');
 (async function testForm() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://<TESTING-WEB-SERVER-IP>/');
+    await driver.get('http://54.83.89.110/');
     await driver.findElement(By.name('name')).sendKeys('Alice');
     await driver.findElement(By.name('email')).sendKeys('alice@example.com');
     await driver.findElement(By.name('role')).sendKeys('Developer');
